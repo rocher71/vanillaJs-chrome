2b505dd320fe2a62d58fd0ddc1b8aa02843b9897
rocher71 <qja086351@naver.com>
2022-03-25 02:10:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+
+function onLoginBtnClick(){
+    console.dir(loginInput.value);
+    console.log("click!");
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
