8ea27f40857dde751006d706da1d3e2cba1f8e8e
rocher71 <qja086351@naver.com>
2022-03-28 02:26:03 +0900

    #5.2 Clock - PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    //padStart를 쓰기 위해서 number -> string
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText= `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // 얘 없이 interval만 하면 refresh 했을 때 1초 기다리고 뜸
