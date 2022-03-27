5ed4f97c05167976bb22702ba3735db4c88daced
rocher71 <qja086351@naver.com>
2022-03-28 01:47:30 +0900

    #4.5 login - Saving Username

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username} keep going!`;
-    //variable을 string안에 넣는 방법
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
