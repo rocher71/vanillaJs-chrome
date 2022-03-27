3131a710e3892e28d9900a826da3fa554a963c4a
rocher71 <qja086351@naver.com>
2022-03-28 01:19:34 +0900

    #4.2 login - Events

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const loginForm = document.querySelector("login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    console.log("username");
+function onLoginSubmit(event){
+    event.preventDefault();
+    //preventDefault() -> stops the default behaviour of any event.
+    console.log(loginInput.value); 
 }
+
+//submit event : enter를 누르거나 버튼 클릭할 때 발생 
+//event가 있을 때 onLoginSubmit을 호출 할건데, 그냥 호출하는게 아니라 인자로 
+// 정보를 전달하면서 호출함 --> onLoginSubmit(information~~); 이런식으로 
+// eventListener function의 첫번째 인자는 항상 어떤 일이 일어났는지에 대한 정보임.
+loginForm.addEventListener("submit", onLoginSubmit);
