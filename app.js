a5e23f3ec90d8acdc2c2cd4890f1dca91ef4e9e1
rocher71 <qja086351@naver.com>
2022-03-28 01:40:18 +0900

    #4.4 login - Getting username

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    //preventDefault() -> stops the default behaviour of any event.
-    console.log(loginInput.value); 
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    //greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username} keep going!`;
+    //variable을 string안에 넣는 방법
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-}
-
-//submit event : enter를 누르거나 버튼 클릭할 때 발생 
-//event가 있을 때 onLoginSubmit을 호출 할건데, 그냥 호출하는게 아니라 인자로 
-// 정보를 전달하면서 호출함 --> onLoginSubmit(information~~); 이런식으로 
-// eventListener function의 첫번째 인자는 항상 어떤 일이 일어났는지에 대한 정보임.
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
