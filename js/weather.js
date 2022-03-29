1aff94ad10d1243f9e80944399a79f7d203b0963
rocher71 <qja086351@naver.com>
2022-03-29 16:25:32 +0900

    #8.1 Weather - Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "276ba02c7abbd4954374f705e61756a1";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live in ", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    //js에서 url 호출한 후 response를 data라는 이름으로 가져옴.
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+            
+    });
 }
 
 function onGeoError(){
